3b7a491d43d4401cdd6b521f7143bf9f1cbc4377
vardineedisatish-dev <vardineedisatish@gmail.com>
2026-02-20 05:55:53 -0800

    implementation of HOF code

--- phase01-core-js-node/practice/caluclator.js
+++ phase01-core-js-node/practice/caluclator.js
@@ -1,2 +1,18 @@
-lat a=10
-let b
+function processArray(array,operation){
+          return array.map(operation);
+}
+
+function double(x){
+        return x*2;
+}
+
+function square(x){
+     return x*x;
+}
+
+const number=[1,2,3,4,5];
+
+console.log("original numbers:",number);
+console.log("double value:",processArray(number,double));
+console.log("square value:", processArray(number,square));
+
