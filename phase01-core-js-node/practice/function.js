2b0d1cab10b1bb7f8bcd9e17bd07ccb228981749
vardineedisatish-dev <vardineedisatish@gmail.com>
2026-02-23 13:49:02 +0000

    commit untracked changes

--- phase01-core-js-node/practice/function.js
+++ phase01-core-js-node/practice/function.js
@@ -1,1 +1,25 @@
+function createCounter(){
+    let count=0;
+    return{
+         increment : function(){
+            count++;       
+            return count;
+         },
+         decrement : function(){
+            count--;
+            return count;
+         },
+         reset : function(){
+            count=0;
+            return count;
+         }
+    };
+}
+
+const counter=createCounter();
+
+console.log(counter.increment());
+console.log(counter.increment());
+console.log(counter.decrement());
+console.log(counter.reset());
 
