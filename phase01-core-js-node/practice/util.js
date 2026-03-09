60c9954ab6c06dab5a23a6250bfcb6392fbe9be6
vardineedisatish-dev <vardineedisatish@gmail.com>
2026-03-09 07:19:14 -0700

    Add async file reading utility function

--- phase01-core-js-node/practice/util.js
+++ phase01-core-js-node/practice/util.js
@@ -1,1 +1,16 @@
 
+const util = require('util');
+const fs = require('fs');
+
+const readFile = util.promisify(fs.readFile);
+
+async function readData() {
+  try {
+    const data = await readFile('sample.txt', 'utf8');
+    console.log(data);
+  } catch (err) {
+    console.error("Error reading file:", err);
+  }
+}
+
+readData();
